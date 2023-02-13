bdbb7dde1d93cc67f3e288e6a53570d1b26cbe57
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2023-02-13 13:48:36 +0200

    LCJS 4.0.1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # JavaScript 3D Box Series rounded edges
 
-![JavaScript 3D Box Series rounded edges](3dBoxRounded.png)
+![JavaScript 3D Box Series rounded edges](3dBoxRounded-darkGold.png)
 
 This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
 
@@ -8,54 +8,61 @@ LightningChart JS is entirely GPU accelerated and performance optimized charting
 
 The demo can be used as an example or a seed project. Local execution requires the following steps:
 
-- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
-- Open the project folder in a terminal:
+-   Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+-   Open the project folder in a terminal:
 
-        npm install              # fetches dependencies
-        npm start                # builds an application and starts the development server
+          npm install              # fetches dependencies
+          npm start                # builds an application and starts the development server
 
-- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+-   The application is available at _http://localhost:8080_ in your browser, webpack-dev-server provides hot reload functionality.
 
 
 ## Description
 
 This example shows an animated 3D Bar Chart with bars coloured dynamically based on their height.
 
-The Chart is rendered using ``BoxSeries3D``, a series type capable of rendering any number of Boxes as well as allowing individual full level modification (location, size, color) at any point during runtime.
+The Chart is rendered using `BoxSeries3D`, a series type capable of rendering any number of Boxes as well as allowing individual full level modification (location, size, color) at any point during runtime.
 
-The example also showcases the rounded edges feature. While enabled by default, it can be adjusted and disabled at any time with ``BoxSeries3D.setRoundedEdges``
+The example also showcases the rounded edges feature. While enabled by default, it can be adjusted and disabled at any time with `BoxSeries3D.setRoundedEdges`
 
 ## BoxSeries3D data format
 
 `BoxSeries3D` supports input data in two different formats, namely:
-- `BoxDataCentered` (specify Box center coordinate and size)
-- `BoxDataBounds` (specify Box start and end coordinates)
 
-On top of this *geometry* information, one can also supply the following optional properties:
+-   `BoxDataCentered` (specify Box center coordinate and size)
+-   `BoxDataBounds` (specify Box start and end coordinates)
 
-- **id**:
-    * By supplying an ID, the Boxes' properties can be modified later (by passing the same *id*).
-    * When modifying a previously added Box, **all** properties (other than *id*) are optional (supplied properties are overridden).
+On top of this _geometry_ information, one can also supply the following optional properties:
+
+-   **id**:
+    -   By supplying an ID, the Boxes' properties can be modified later (by passing the same _id_).
+    -   When modifying a previously added Box, **all** properties (other than _id_) are optional (supplied properties are overridden).
+
 ```typescript
 // Initial Box definition (BoxDataCentered format).
-BoxSeries3D.invalidateData([{
-    xCenter: 0,
-    yCenter: 0,
-    zCenter: 0,
-    xSize: 1,
-    ySize: 1,
-    zSize: 1,
-    id: 'box-#0'
-}])
+BoxSeries3D.invalidateData([
+    {
+        xCenter: 0,
+        yCenter: 0,
+        zCenter: 0,
+        xSize: 1,
+        ySize: 1,
+        zSize: 1,
+        id: 'box-#0',
+    },
+])
 // Modify 'yCenter' property only.
-BoxSeries3D.invalidateData([{
-    id: 'box-#0',
-    yCenter: 5
-}])
+BoxSeries3D.invalidateData([
+    {
+        id: 'box-#0',
+        yCenter: 5,
+    },
+])
 ```
 
-- **color**:
-    * Assigns a *Color* to the Box to be used when individual coloring mode is enabled.
+-   **color**:
+    -   Assigns a _Color_ to the Box to be used when individual coloring mode is enabled.
+
 ```typescript
 // Add Boxes with individual Colors.
 BoxSeries3D.invalidateData([{
@@ -66,8 +73,9 @@ BoxSeries3D.invalidateData([{
 BoxSeries3D.setFillStyle( new IndividualPointFill() )
 ```
 
-- **value**:
-    * Assigns a *look-up value* to the Box to be used when look-up coloring mode is enabled.
+-   **value**:
+    -   Assigns a _look-up value_ to the Box to be used when look-up coloring mode is enabled.
+
 ```typescript
 // Define Color Look-Up-Table.
 const lut = new LUT( {
@@ -101,27 +109,27 @@ BoxSeries3D.setFillStyle( new PalettedFill({ lut }) )
 
 If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
 
-Official [API documentation][1] can be found on [Arction][2] website.
+Official [API documentation][1] can be found on [LightningChart][2] website.
 
 If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
 
-If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+If you think you found a bug in the LightningChart JavaScript library, please contact support@lightningchart.com.
 
-Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@lightningchart.com.
 
 [0]: https://github.com/Arction/
-[1]: https://www.arction.com/lightningchart-js-api-documentation/
-[2]: https://www.arction.com
+[1]: https://lightningchart.com/lightningchart-js-api-documentation/
+[2]: https://lightningchart.com
 [3]: https://stackoverflow.com/questions/tagged/lightningchart
-[4]: https://www.arction.com/support-services/
+[4]: https://lightningchart.com/support-services/
 
-© Arction Ltd 2009-2020. All rights reserved.
+© LightningChart Ltd 2009-2022. All rights reserved.
 
 
-[3D chart]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/chart3d.html
-[3D box series]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/boxseries3d.html
-[Box data centered]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/interfaces/boxdatacentered.html
-[Box data bounds]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/interfaces/boxdatabounds.html
-[Paletted Fill Style]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/palettedfill.html
-[LUT]: https://www.arction.com/lightningchart-js-api-documentation/v3.4.0/classes/lut.html
+[3D chart]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/Chart3D.html
+[3D box series]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/BoxSeries3D.html
+[Box data centered]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/interfaces/BoxDataCentered.html
+[Box data bounds]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/interfaces/BoxDataBounds.html
+[Paletted Fill Style]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/PalettedFill.html
+[LUT]: https://lightningchart.com/lightningchart-js-api-documentation/v4.0.0/classes/LUT.html
 
--- src/index.js
+++ src/index.js
@@ -4,87 +4,80 @@
 // Import LightningChartJS
 const lcjs = require('@arction/lcjs')
 
+// Import xydata
+const xydata = require('@arction/xydata')
+
 // Extract required parts from LightningChartJS.
 const {
     lightningChart,
     AxisScrollStrategies,
     PalettedFill,
-    ColorRGBA,
     LUT,
     UILayoutBuilders,
     UIOrigins,
     UIElementBuilders,
-    Themes
+    regularColorSteps,
+    Themes,
 } = lcjs
 
-const {
-    createWaterDropDataGenerator
-} = require('@arction/xydata')
+const { createWaterDropDataGenerator } = xydata
 
+const chart3D = lightningChart()
+    .Chart3D({
+        // theme: Themes.darkGold
+    })
+    .setTitle('BoxSeries3D with rounded edges enabled')
 
+chart3D.getDefaultAxisY().setScrollStrategy(AxisScrollStrategies.expansion).setTitle('Height')
 
-const chart3D = lightningChart().Chart3D( {
-    disableAnimations: true,
-    // theme: Themes.darkGold
-} )
-    .setTitle( 'BoxSeries3D with rounded edges enabled' )
+chart3D.getDefaultAxisX().setTitle('X')
 
-chart3D.getDefaultAxisY()
-    .setScrollStrategy( AxisScrollStrategies.expansion )
-    .setTitle( 'Height' )
-
-chart3D.getDefaultAxisX()
-    .setTitle( 'X' )
-
-chart3D.getDefaultAxisZ()
-    .setTitle( 'Z' )
+chart3D.getDefaultAxisZ().setTitle('Z')
 
 const boxSeries = chart3D.addBoxSeries()
 const resolution = 10
 
 // Create Color Look-Up-Table and FillStyle
-const lut = new LUT( {
-    steps: [
-        { value: 0, color: ColorRGBA( 0, 0, 0 ) },
-        { value: 30, color: ColorRGBA( 255, 255, 0 ) },
-        { value: 45, color: ColorRGBA( 255, 204, 0 ) },
-        { value: 60, color: ColorRGBA( 255, 128, 0 ) },
-        { value: 100, color: ColorRGBA( 255, 0, 0 ) }
-    ],
-    interpolate: true
-} )
+const theme = chart3D.getTheme()
+const lut = new LUT({
+    steps: regularColorSteps(0, 100, theme.examples.intensityColorPalette),
+    interpolate: true,
+})
 
 boxSeries
-    .setFillStyle( new PalettedFill( { lut, lookUpProperty: 'y' } ) )
+    .setFillStyle(new PalettedFill({ lut, lookUpProperty: 'y' }))
     // Specify edge roundness.
     // For applications with massive amounts of small Boxes, it is wise to disable for performance benefits.
-    .setRoundedEdges( 0.4 )
+    .setRoundedEdges(0.4)
 
 // Add LegendBox to chart.
-const legend = chart3D.addLegendBox()
+const legend = chart3D
+    .addLegendBox()
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-width',
-        maxWidth: 0.30,
+        maxWidth: 0.3,
     })
     .add(chart3D)
 
 // Generate height map data.
 createWaterDropDataGenerator()
-    .setRows( resolution )
-    .setColumns( resolution )
+    .setRows(resolution)
+    .setColumns(resolution)
     .generate()
-    .then( waterdropData => {
+    .then((waterdropData) => {
         let t = 0
         const step = () => {
             const result = []
-            for ( let x = 0; x < resolution; x++ ) {
-                for ( let y = 0; y < resolution; y++ ) {
+            for (let x = 0; x < resolution; x++) {
+                for (let y = 0; y < resolution; y++) {
                     const s = 1
                     const height = Math.max(
                         waterdropData[y][x] +
-                        50 * Math.sin( ( t + x * .50 ) * Math.PI / resolution ) +
-                        20 * Math.sin( ( t + y * 1.0 ) * Math.PI / resolution ), 0 )
+                            50 * Math.sin(((t + x * 0.5) * Math.PI) / resolution) +
+                            20 * Math.sin(((t + y * 1.0) * Math.PI) / resolution),
+                        0,
+                    )
                     const box = {
                         xCenter: x,
                         yCenter: height / 2,
@@ -93,72 +86,63 @@ createWaterDropDataGenerator()
                         ySize: height,
                         zSize: s,
                         // Specify an ID for each Box in order to modify it during later frames, instead of making new Boxes.
-                        id: String( result.length ),
+                        id: String(result.length),
                     }
-                    result.push( box )
+                    result.push(box)
                 }
             }
-        
-            boxSeries
-                .invalidateData( result )
-        
+
+            boxSeries.invalidateData(result)
+
             t += 0.1
-            requestAnimationFrame( step )
+            requestAnimationFrame(step)
         }
         step()
     })
 
-
-
 // Animate Camera movement from file.
 ;(async () => {
-    const cameraAnimationData = await (
-        fetch( document.head.baseURI + 'examples/assets/0907/camera.json' )
-            .then( r => r.json() )
-    )
-    if ( ! cameraAnimationData ) {
+    const cameraAnimationData = await fetch(document.head.baseURI + 'examples/assets/0907/camera.json').then((r) => r.json())
+    if (!cameraAnimationData) {
         console.log(`No Camera animation data.`)
         return
     }
     console.log(`Loaded Camera animation data.`)
     let frame = 0
     const nextFrame = () => {
-        if ( cameraAnimationEnabledCheckbox.getOn() ) {
+        if (cameraAnimationEnabledCheckbox.getOn()) {
             const { cameraLocation } = cameraAnimationData.frames[Math.floor(frame) % cameraAnimationData.frames.length]
-            chart3D.setCameraLocation( cameraLocation )
+            chart3D.setCameraLocation(cameraLocation)
             frame += 1.5
         }
-        requestAnimationFrame( nextFrame )
+        requestAnimationFrame(nextFrame)
     }
-    requestAnimationFrame( nextFrame )
+    requestAnimationFrame(nextFrame)
 })()
-
-
 
 // * UI controls *
 const group = chart3D.addUIElement(UILayoutBuilders.Column)
 group
-    .setPosition( { x: 0, y: 100 } )
-    .setOrigin( UIOrigins.LeftTop )
-    .setMargin( 10 )
-    .setPadding( 4 )
+    .setPosition({ x: 0, y: 100 })
+    .setOrigin(UIOrigins.LeftTop)
+    .setMargin(10)
+    .setPadding(4)
     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
     .setAutoDispose({
         type: 'max-height',
-        maxHeight: 0.30,
+        maxHeight: 0.3,
     })
 
-
 // Add UI control for toggling camera animation.
-const handleCameraAnimationToggled = ( state ) => {
-    cameraAnimationEnabledCheckbox.setText( state ? 'Disable camera animation' : 'Enable camera animation' )
-    if ( cameraAnimationEnabledCheckbox.getOn() !== state ) {
-        cameraAnimationEnabledCheckbox.setOn( state )
+const handleCameraAnimationToggled = (state) => {
+    cameraAnimationEnabledCheckbox.setText(state ? 'Disable camera animation' : 'Enable camera animation')
+    if (cameraAnimationEnabledCheckbox.getOn() !== state) {
+        cameraAnimationEnabledCheckbox.setOn(state)
     }
 }
-const cameraAnimationEnabledCheckbox = group.addElement( UIElementBuilders.CheckBox )
-cameraAnimationEnabledCheckbox.onSwitch((_, state) => handleCameraAnimationToggled( state ))
-handleCameraAnimationToggled( true )
+const cameraAnimationEnabledCheckbox = group.addElement(UIElementBuilders.CheckBox)
+cameraAnimationEnabledCheckbox.onSwitch((_, state) => handleCameraAnimationToggled(state))
+handleCameraAnimationToggled(true)
 chart3D.onBackgroundMouseDrag(() => {
-    handleCameraAnimationToggled( false )
+    handleCameraAnimationToggled(false)
 })
