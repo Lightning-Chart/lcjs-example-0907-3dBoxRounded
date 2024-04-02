3cd6acc76bbed40bdebf22eebe9016cad49e7c1e
Ilkka Kuosmanen <ilkka.kuosmanen@lightningchart.com>
2024-04-02 11:50:45 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -102,7 +102,9 @@ createWaterDropDataGenerator()
 
 // Animate Camera movement from file.
 ;(async () => {
-    const cameraAnimationData = await fetch(document.head.baseURI + 'examples/assets/0907/camera.json').then((r) => r.json())
+    const cameraAnimationData = await fetch(
+        new URL(document.head.baseURI).origin + new URL(document.head.baseURI).pathname + 'examples/assets/0907/camera.json',
+    ).then((r) => r.json())
     if (!cameraAnimationData) {
         console.log(`No Camera animation data.`)
         return
