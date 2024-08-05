619a28a23ade7963fb229a7a4ef13e8440cf1cb8
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-08-05 09:53:29 +0300

    v6.0.0

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
  * LightningChartJS example that showcases BoxSeries3D with rounded edges.
  */
 // Import LightningChartJS
-const lcjs = require('@arction/lcjs')
+const lcjs = require('@lightningchart/lcjs')
 
 // Import xydata
-const xydata = require('@arction/xydata')
+const xydata = require('@lightningchart/xydata')
 
 // Extract required parts from LightningChartJS.
 const {
